e386da30d1852983d99a5ce304832348143e0623
alex4321 <gaussmake@gmail.com>
2023-11-04 11:07:54 +0400

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+import os
 
 
 NAME = "langchain_openai_limiter"
-VERSION = "0.0.2"
+VERSION = "0.0.2.1"
 DESCRIPTION = """Wrapper for Langchain & OpenAI api calls which use OpenAI headers to deal with TPM & RPM.
 Also allow to use multiple keys from different organizations."""
 AUTHOR = "Alexander Pozharskii"
@@ -22,12 +23,17 @@ DEV_REQUIRES = [
     "numpy>=1.26.1"
 ]
 URL = "https://github.com/alex4321/langchain-openai-limiter"
+LONG_DESCRIPTION_FNAME = os.path.join(os.path.dirname(__file__), "README.md")
+with open(LONG_DESCRIPTION_FNAME, "r", encoding="utf-8") as src:
+    LONG_DESCRIPTION = src.read()
 
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     packages=PACKAGES,
